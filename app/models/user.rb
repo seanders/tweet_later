d7ec0bd0470dc200c3bdfec8e5c92ea9cf6a4eb8
Apprentice <apprentice@devbootcamp.com>
2013-05-08 18:32:59 -0700

    boostrapped

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
   def tweet(time, status)
     tweet = self.tweets.create!(:status => status)
-    TweetWorker.perform_in(time.minutes, tweet.id)
+    TweetWorker.perform_in(time.to_i.minutes, tweet.id)
     # TweetWorker.perform_async(tweet.id)
   end
 end
